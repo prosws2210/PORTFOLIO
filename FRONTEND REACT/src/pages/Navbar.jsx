86d24455e47786873c60cf8803f910428c3af556
prosws2210 <proshaks22@gmail.com>
2024-06-08 19:03:44 +0530

    Few changes made

--- FRONTEND REACT/src/pages/Navbar.jsx
+++ FRONTEND REACT/src/pages/Navbar.jsx
@@ -13,16 +13,8 @@ const Navbar = () => {
             <ul className="flex space-x-4">
               <li><Link to="/" className="text-white hover:text-gray-300">Home</Link></li>
               <li><Link to="/about" className="text-white hover:text-gray-300">About</Link></li>
-              <li><Link to="/services" className="text-white hover:text-gray-300">Services</Link></li>
               <li><Link to="/contact" className="text-white hover:text-gray-300">Contact</Link></li>
             </ul>
-          </div>
-          <div className="flex items-center">
-            <Link to="/login">
-              <button className="border-2 rounded-full px-4 py-1 border-blue-500 text-sm font-medium text-blue-700 hover:bg-blue-500 hover:text-white">
-                Login / Register
-              </button>
-            </Link>
           </div>
         </div>
       </div>
